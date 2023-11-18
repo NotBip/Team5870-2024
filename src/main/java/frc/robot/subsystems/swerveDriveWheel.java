1b9f119a0fd3f2222b10bdaf679e401714ea123a
NotBip <hamadmohamed0009@gmail.com>
2023-11-17 20:35:46 -0500

    Temp Change

---
+++ src/main/java/frc/robot/subsystems/swerveDriveWheel.java
@@ -0,0 +1,16 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.math.controller.PIDController;
+// import edu.wpi.first.wpilibj.PIDOutput;
+
+
+public class swerveDriveWheel {
+ 
+    public PIDController directionController = new PIDController(0, 0, 0);
+    // public PIDOutput directionOutput;
+
+    public swerveDriveWheel(){
+        // directionController.set
+    }
+
+}
