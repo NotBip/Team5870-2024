525c42a160fbbb6001b27d0ae2dac26aaaefe48c
BlaBlak2 <89756590+BlaBlak2@users.noreply.github.com>
2023-11-21 20:55:37 -0500

    Co-authored-by: NotBip <NotBip@users.noreply.github.com> Co-authored-by: Crield <Crield@users.noreply.github.com>

--- src/main/java/frc/robot/commands/Commands.java
+++ src/main/java/frc/robot/commands/Commands.java
@@ -1,13 +1,9 @@
 package frc.robot.commands;
 
-import edu.wpi.first.wpilibj.GenericHID.RumbleType;
 import edu.wpi.first.wpilibj2.command.*;
 import frc.robot.Robot;
-import static frc.robot.Robot.io;
 
 public class Commands {
 	// Drive Train
 	public static final CommandBase emergencyStop = new InstantCommand(() -> Robot.driveTrain.stop(), Robot.driveTrain);
-
 }
- 
