177be8f09055be2256a3cd1fb58b6667080db9d7
NotBip <hamadmohamed0009@gmail.com>
2024-02-24 01:06:52 -0500

    climber update

--- src/main/java/frc/robot/subsystems/Climber.java
+++ src/main/java/frc/robot/subsystems/Climber.java
@@ -2,6 +2,8 @@ package frc.robot.subsystems;
 import frc.robot.Constants.climberConstants;
 import frc.robot.Constants.climberConstants.*;
 import com.revrobotics.CANSparkMax;
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.SparkPIDController;
 
 import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
@@ -13,6 +15,10 @@ public class Climber extends SubsystemBase {
     private CANSparkMax followerMotor; 
     DigitalInput bottomLimitSwitch;
     DigitalInput topLimitSwitch;
+    private SparkPIDController m_pidController;
+    private RelativeEncoder m_encoder;
+    public double kP, kI, kD, kIz, kFF, kMaxOutput, kMinOutput;
+
 
 
     public Climber() { 
@@ -28,10 +34,46 @@ public class Climber extends SubsystemBase {
 
         // Follower motor follows Leader Motor
         followerMotor.follow(leaderMotor);
+
+        m_pidController = leaderMotor.getPIDController();
+        m_encoder = leaderMotor.getEncoder();
+                
+        kP = 0.1; 
+        kI = 0;
+        kD = 0; 
+        kIz = 0; 
+        kFF = 0; 
+        kMaxOutput = .5; 
+        kMinOutput = -.5;
+
+        // set PID coefficients
+        m_pidController.setP(kP);
+        m_pidController.setI(kI);
+        m_pidController.setD(kD);
+        m_pidController.setIZone(kIz);
+        m_pidController.setFF(kFF);
+        m_pidController.setOutputRange(kMinOutput, kMaxOutput);
+
+        // display PID coefficients on SmartDashboard
+        // SmartDashboard.putNumber("P Gain", kP);
+        // SmartDashboard.putNumber("I Gain", kI);
+        // SmartDashboard.putNumber("D Gain", kD);
+        // SmartDashboard.putNumber("I Zone", kIz);
+        // SmartDashboard.putNumber("Feed Forward", kFF);
+        // SmartDashboard.putNumber("Max Output", kMaxOutput);
+        // SmartDashboard.putNumber("Min Output", kMinOutput);
+        // SmartDashboard.putNumber("Set Rotations", 0);
+
+
+
     }
 
     public void moveArm(double motorSpeed) { 
         leaderMotor.set(motorSpeed);
+    }
+
+    public void setPosition(double rotations) { 
+        m_pidController.setReference(rotations, CANSparkMax.ControlType.kPosition);
     }
 
     
