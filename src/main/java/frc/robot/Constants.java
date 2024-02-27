f85b89ca52018076ad626b204a28973bf8f143bc
NotBip <hamadmohamed0009@gmail.com>
2024-02-27 12:41:32 -0500

    Pneumatics Class

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -32,7 +32,13 @@ public final class Constants {
         public static int armMotor = 9; 
     }
 
-
+    public static final class PneumaticsConstants { 
+        public static int solenoidExtend1ID = 1; 
+        public static int solenoidExtend2ID = 2; 
+        public static int solenoidDetract1ID = 3; 
+        public static int solenoidDetract2ID = 4; 
+        public static int compressorID = 0; 
+    }
 
 
     // May be removed later. 
